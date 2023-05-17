5b7451444cbd9189b1c2360e51b5a087bc3ed9db
Hudson Marinho <hudsonmarinhodev@gmail.com>
2023-05-17 02:20:45 -0300

    feat(home): created home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Wrapper from "../../components/Wrapper";
+import Header from "../../components/Header";
+import ListPokemons from "../../components/ListPokemons";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Header />
+
+      <Wrapper>
+      </Wrapper>
+    </>
+  );
+};
+
+export default Home;
